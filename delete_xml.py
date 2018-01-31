d8cefba42cae5476c3c47db9e76b910917d309e7
ZhuMingren <zhumingren@secretlisa.com>
2018-01-31 15:54:11 +0800

    Remove前缀

--- delete_xml.py
+++ delete_xml.py
@@ -3,9 +3,10 @@
 import lxml
 from lxml import etree, objectify
 
-def delete_element(file_name, prefix_tuple, remove = False):
+
+def delete_element(file_name, prefix_tuple, remove=False):
     tree = lxml.etree.parse(file_name)
-    res = tree.xpath('/resources')[0]   # 获取bndbox元素的内容
+    res = tree.xpath('/resources')[0]  # 获取bndbox元素的内容
     for ss in res.getchildren():  # 便利bndbox元素下的子元素
         name = ss.get("name")
         name_divided = str(name).split("_");
@@ -14,18 +15,23 @@ def delete_element(file_name, prefix_tuple, remove = False):
             name_prefix = name_divided[0]
             if prefix_tuple.__contains__(name_prefix):
                 is_contain = True
+                if not remove:
+                    prefix_len = len(name_prefix) + 1
+                    name_new = name[prefix_len:len(name)]
+                    ss.set("name", name_new)
 
         # print "%s contains %s , name_prefix = %s" % (name, str(is_contain), name_prefix)
         if remove:
             if is_contain:
                 res.remove(ss)
                 print "delete element name = " + name
-        else: # 保留这些标签
+        else:  # 保留这些标签
             if not is_contain:
                 res.remove(ss)
         ss.tail = "\n"
     tree.write(file_name, encoding='utf-8')
     append_header(file_name)
+
 
 def append_header(file_name):
     file_header = '<?xml version="1.0" encoding="UTF-8"?>\n'
@@ -35,6 +41,7 @@ def append_header(file_name):
     file_write = open(file_name, "w")
     file_write.write(file_header + file_body)
     file_write.close()
+
 
 if __name__ == "__main__":
     print "main"
