1f94870bd8552b41212b5679c05dd5b2a412d258
ZhuMingren <zhumingren@secretlisa.com>
2018-01-31 13:55:20 +0800

    Add修改一个文件的功能

--- delete_xml.py
+++ delete_xml.py
@@ -3,20 +3,30 @@
 import lxml
 from lxml import etree, objectify
 
-tree = lxml.etree.parse("strings.xml")
+file_name = "values-de/strings.xml"
+prefix = "kkfriends"
 
-def prn_obj(obj):
-    print '\n'.join(['%s:%s' % item for item in obj.__dict__.items()])
+def delete_element(file_name, prefix_tuple, remove = False):
+    tree = lxml.etree.parse(file_name)
+    res = tree.xpath('/resources')[0]   # 获取bndbox元素的内容
+    for ss in res.getchildren():  # 便利bndbox元素下的子元素
+        name = ss.get("name")
+        name_divided = str(name).split("_");
+        is_contain = False
+        if len(name_divided) > 1:
+            name_prefix = name_divided[0]
+            if prefix_tuple.__contains__(name_prefix):
+                is_contain = True
 
-res = tree.xpath('/resources')[0]   # 获取bndbox元素的内容
-for ss in res.getchildren():  # 便利bndbox元素下的子元素
-    #print ss.text   # string类型
-    #prn_obj(ss)
-    name = ss.get("name")
-    if str(name).__contains__("getfriends"):
-        print name
-        res.remove(ss)
-#etree.ElementTree(tree.ElementTree()).write("strings.xml", pretty_print=True)
-#print tree
+        print "%s contains %s , name_prefix = %s" % (name, str(is_contain), name_prefix)
+        if remove:
+            if is_contain:
+                res.remove(ss)
+        else: # 保留这些标签
+            if not is_contain:
+                res.remove(ss)
 
-tree.write("strings.xml")
+    tree.write(file_name, encoding='utf-8')
+
+prefix_tuple = ("flamingo", "friendsfinder", "getfriends", "kkfriends")
+delete_element(file_name, prefix_tuple, True)
