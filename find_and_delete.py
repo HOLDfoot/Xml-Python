937edd6fc018fce95afb99ec6b0bd6539d06480d
ZhuMingren <zhumingren@secretlisa.com>
2018-01-31 14:17:47 +0800

    before copy into res

--- find_and_delete.py
+++ find_and_delete.py
@@ -4,6 +4,11 @@ import find_strings_file as find_file
 import delete_xml as delete_element
 
 prefix_tuple = ("flamingo", "friendsfinder", "getfriends", "kkfriends")
+flamingo_tuple = ("flamingo")
+friendsfinder_tuple = ("friendsfinder")
+getfriends_tuple = ("getfriends")
+kkfriendstuple = ("kkfriends")
+
 finded_files = find_file.get_finded_file()
 for file in finded_files:
     print file
