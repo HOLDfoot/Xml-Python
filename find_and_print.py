6ea91b8574b6defb8ab9934deff872b7b9f83125
ZhuMingren <zhumingren@secretlisa.com>
2018-02-02 15:44:01 +0800

    Add格式化输出

--- find_and_print.py
+++ find_and_print.py
@@ -1,5 +1,6 @@
 # encoding=UTF-8
 
+import sys
 import find_strings_file as find_file
 import print_xml as print_element
 
@@ -9,11 +10,18 @@ friendsfinder_tuple = ("friendsfinder",)
 getfriends_tuple = ("getfriends",)
 kkfriendstuple = ("kkfriends",)
 
-name_contain = "_signup_title_"
+if len(sys.argv) >= 2:
+    print_detail = int(sys.argv[1])
+else:
+    print_detail = 1
 
+name_contain = "feedback"
+mini_bounds = 18
+
+print name_contain + "在以下文件中长度应该小于" + str(mini_bounds)
 finded_files = find_file.get_finded_file()
 for file in finded_files:
     #print file
-    print_element.print_element(file, name_contain)
-print print_element.max_list
-print print_element.all_max
+    print_element.print_element(file, name_contain, mini_bounds, print_detail)
+# print print_element.max_list
+# print print_element.all_max
--- print_xml.py
+++ print_xml.py
@@ -8,7 +8,7 @@ max_list = []
 global all_max
 all_max = 0
 
-def print_element(file_name, name_contain, mini_bounds=35):
+def print_element(file_name, name_contain, mini_bounds=35, print_detail=1):
     tree = lxml.etree.parse(file_name)
     res = tree.xpath('/resources')[0]  # 获取bndbox元素的内容
     had_print = False
@@ -18,21 +18,21 @@ def print_element(file_name, name_contain, mini_bounds=35):
         if str(element_name).__contains__(name_contain):
             element_text = ss.text
             element_text_len = len(element_text)
-            if element_text_len > mini_bounds:
+            if element_text_len >= mini_bounds:
                 if not had_print:
-                    print file_name
+                    print file_name[5:len(file_name)]
                     had_print = True
-                print element_name
-                print element_text_len
-                max_text_len = max_text_len if max_text_len > element_text_len else element_text_len
-    if max_text_len != 0:
-        print "max _signup_title_ = " + str(max_text_len)
-        file_name = file_name[5:len(file_name)]
-        #max_model = MaxModel(file_name, max_text_len)
-        max_model =  "(" + file_name + "," + str(max_text_len) + ")"
-        max_list.append(max_model)
-        global all_max
-        all_max = all_max if all_max > max_text_len else max_text_len
+                if print_detail != 0:
+                    print element_name
+                    print element_text_len
+    #             max_text_len = max_text_len if max_text_len > element_text_len else element_text_len
+    # if max_text_len != 0:
+    #     # print "max _signup_title_ = " + str(max_text_len)
+    #     file_name = file_name[5:len(file_name)]
+    #     max_model =  "(" + file_name + "," + str(max_text_len) + ")"
+    #     max_list.append(max_model)
+    #     global all_max
+    #     all_max = all_max if all_max > max_text_len else max_text_len
 
 
 
